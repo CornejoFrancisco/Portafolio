cb15be566e89440c020ad818076c6d31111504b7
CornejoFrancisco <francisco.cornejo1978@gmail.com>
2024-07-04 11:54:49 -0300

    Portafolio

--- portafolio/components/icon_button.py
+++ portafolio/components/icon_button.py
@@ -1,13 +1,18 @@
 import reflex as rx
 
-
 def icon_button(icon: str, url: str, text="", solid=False) -> rx.Component:
+    button_style = "solid" if solid else "surface"
+    
+    # La variable button_style determina el estilo del botón: solid o surface
+    button = rx.button(
+        rx.icon(icon),
+        text,
+        class_name="icon-button",
+        variant="outline"
+    )
+    
     return rx.link(
-        rx.button(
-            rx.icon(icon),
-            text,
-            variant="solid" if solid else "surface"
-        ),
+        button,
         href=url,
         is_external=True
     )
